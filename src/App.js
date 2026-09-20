631ea1469516f44ca4240545b33b2a59a21a5167
Laux0 <laura.rahmati@gmail.com>
2026-09-20 17:32:04 +0200

    Created a Weather component and a CSS file for it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css";
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by Laura Rahmati and is open-sourced on {""}
         <a
