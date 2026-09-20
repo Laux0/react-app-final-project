cd49ab6735ba6cdca881bc2aecb1e62d5ac067bc
Laux0 <laura.rahmati@gmail.com>
2026-09-20 16:27:36 +0200

    Added footer with link to Github

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by Laura Rahmati and is open-sourced on {""}
+        <a
+          href="https://github.com/Laux0/react-app-final-project"
+          target="_blank"
+          rel="moreferrer"
+        >
+          Github.
+        </a>
+      </footer>
     </div>
   );
 }
