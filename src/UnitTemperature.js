f283666b1e7b1b30aa6c07e5d2a481c3cff1dc01
Laux0 <laura.rahmati@gmail.com>
2026-09-22 22:28:27 +0200

    Added Fahrenheit and option for selecting unit

--- src/UnitTemperature.js
+++ src/UnitTemperature.js
@@ -1,11 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./UnitTemperature.css";
 
 export default function UnitTemperature(props) {
-  return (
-    <div className="current-value">
-      <span className="main-temperature">{Math.round(props.UnitTemp)}</span>
-      <span className="unit">°C</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="current-value">
+        <span className="main-temperature">{Math.round(props.UnitTemp)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="#" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    let fahrenheit = (props.UnitTemp * 9) / 5 + 32;
+    return (
+      <div className="current-value">
+        <span className="main-temperature">{Math.round(fahrenheit)}</span>
+        <span className="unit">
+          <a href="#" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
