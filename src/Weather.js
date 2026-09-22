26304717bd8f40bf81d60927583875611c41e311
Laux0 <laura.rahmati@gmail.com>
2026-09-22 16:43:05 +0200

    used React states and set up temperature with API data

--- src/Weather.js
+++ src/Weather.js
@@ -1,48 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Weather.css";
+import axios from "axios";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Search for city.."
-              className="form-control"
-            />
+  const [ready, setReady] = useState(false);
+  const [temperature, setTemperature] = useState(null);
+
+  function handleResponse(response) {
+    setTemperature(response.data.temperature.current);
+    setReady(true);
+  }
+
+  if (ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search for city.."
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
           </div>
-          <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
-          </div>
-        </div>
-      </form>
-      <div className="Forecast">
-        <h1>Lisbon</h1>
-        <ul>
-          <li>Wednesday 7:00</li>
-          <li>Mostly cloudy</li>
-        </ul>
-        <div className="row">
-          <div className="col-6">
-            <img
-              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="cloudy icon"
-              className="today-icon"
-            />
-            <span className="main-temperature">6</span>
-            <span className="unit">°C</span>
-          </div>
-          <div className="col-6">
-            <ul>
-              <li>Precipitation 15%</li>
-              <li>Humidity 72%</li>
-              <li>Wind 13km/h</li>
-            </ul>
+        </form>
+        <div className="Forecast">
+          <h1>Lisbon</h1>
+          <ul>
+            <li>Wednesday 7:00</li>
+            <li>Mostly cloudy</li>
+          </ul>
+          <div className="row">
+            <div className="col-6">
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                alt="cloudy icon"
+                className="today-icon"
+              />
+              <span className="main-temperature">
+                {Math.round(temperature)}
+              </span>
+              <span className="unit">°C</span>
+            </div>
+            <div className="col-6">
+              <ul>
+                <li>Precipitation 15%</li>
+                <li>Humidity 72%</li>
+                <li>Wind 13km/h</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "83bco8b8afca3aft80c7a9a59f08542a";
+    let city = "Lisbon";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return "Loading...";
+  }
 }
