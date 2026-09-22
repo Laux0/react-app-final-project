8c3695a7a42ef21c09c7d292215a8f45ae48aaf1
Laux0 <laura.rahmati@gmail.com>
2026-09-22 17:31:57 +0200

    Added API weather details

--- src/Weather.js
+++ src/Weather.js
@@ -4,10 +4,16 @@ import axios from "axios";
 
 export default function Weather() {
   const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
+  const [weatherData, setWeatherData] = useState({});
 
   function handleResponse(response) {
-    setTemperature(response.data.temperature.current);
+    setWeatherData({
+      temperature: response.data.temperature.current,
+      wind: response.data.wind.speed,
+      city: response.data.city,
+      humidity: response.data.temperature.humidity,
+      description: response.data.condition.description,
+    });
     setReady(true);
   }
 
@@ -29,10 +35,10 @@ export default function Weather() {
           </div>
         </form>
         <div className="Forecast">
-          <h1>Lisbon</h1>
+          <h1>{weatherData.city}</h1>
           <ul>
             <li>Wednesday 7:00</li>
-            <li>Mostly cloudy</li>
+            <li>{weatherData.description}</li>
           </ul>
           <div className="row">
             <div className="col-6">
@@ -42,15 +48,15 @@ export default function Weather() {
                 className="today-icon"
               />
               <span className="main-temperature">
-                {Math.round(temperature)}
+                {Math.round(weatherData.temperature)}
               </span>
               <span className="unit">°C</span>
             </div>
             <div className="col-6">
               <ul>
-                <li>Precipitation 15%</li>
-                <li>Humidity 72%</li>
-                <li>Wind 13km/h</li>
+                <li>Weather Details:</li>
+                <li>Humidity {weatherData.humidity}%</li>
+                <li>Wind {weatherData.wind}km/h</li>
               </ul>
             </div>
           </div>
